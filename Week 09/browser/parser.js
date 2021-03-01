6f76774ea832e9fade21d54931de0b235605e00f
Sim <sim@snorl.ax>
2021-03-02 00:07:06 +0800

    9.11. CSS计算 | 选择器与元素的匹配

--- Week 09/browser/parser.js
+++ Week 09/browser/parser.js
@@ -1,4 +1,5 @@
 const css = require("css")
+const { match } = require("assert")
 
 // 当前token
 let currentToken = null
@@ -14,8 +15,37 @@ function addCSSRules(text) {
     rules.push(...ast.stylesheet.rules)
 }
 
+function match(element, selector) {
+
+}
+
 function computeCSS(element) {
     var elements = stack.slice().reverse()
+    if(!element.computedStyle)
+        element.computedStyle = {}
+    
+        for(let rule of rules) {
+            var selectorParts = rule.selectors[0].split(" ").reverse()
+
+            if(!match(element, selectorParts[0]))
+                continue
+
+            let matched = false
+
+            var j = 1
+            for(var i = 0; i < elements.length; i++) {
+                if(match(elements[i], selectorParts[j])) {
+                    j++
+                }
+            }
+
+            if(j >= selectorParts.length)
+                matched = true
+            
+            if(matched) {
+                console.log("Element", element, "matched rule", rule)
+            }
+        }
 }
 
 //输出token

5abb4314acb45f1b3c619ecd92710918838451a7
Sim <sim@snorl.ax>
2021-03-01 21:23:02 +0800

    9.6. HTML解析 | 用token构建DOM树

--- Week 09/browser/parser.js
+++ Week 09/browser/parser.js
@@ -1,10 +1,49 @@
 // 当前token
 let currentToken = null
+let currentAttribute = null
+let currentTextNode = null
+
+let stack = [{type: "document", children: []}]
 
 //输出token
 function emit(token) {
-    //if(token.type!="text")
-    console.log(token)
+    if(token.type==="text")
+        return
+
+    let top = stack[stack.length - 1]
+    
+    if(token.type == "startTag") {
+        let element = {
+            type: "element",
+            children: [],
+            attributes: []
+        }
+
+        element.tagName = token.tagName
+
+        for(let p in token) {
+            if(p != "type" && p != "tagName")
+                element.attributes.push({
+                    name: p,
+                    value: token[p]
+                })
+        }
+
+        top.children.push(element)
+        element.parent = top
+
+        if(!token.isSelfClosing)
+            stack.push(element)
+
+        currentTextNode = null
+    } else if(token.type == "endTag") {
+        if(top.tagName != token.tagName) {
+            throw new Error("Tag start end doesn't match!")
+        } else {
+            stack.pop()
+        }
+        currentTextNode = null
+    }
 }
 
 const EOF = Symbol("EOF")
@@ -45,23 +84,6 @@ function tagOpen(c) {
     return
 }
 
-function endTagOpen(c) {
-    // 字母
-    if(c.match(/^[a-zA-Z]$/)) {
-        currentToken = {
-            type: "endTag",
-            tagName: ""
-        }
-        return tagName(c)
-    } else if(c == ">") {
-
-    } else if(c == EOF) {
-
-    } else {
-
-    }
-}
-
 function tagName(c) {
     // 空白，标签名字结束，找属性
     if(c.match(/^[\t\n\f ]$/)) {
@@ -89,23 +111,176 @@ function beforeAttributeName(c) {
     if(c.match(/^[\t\n\f ]$/)) {
         return beforeAttributeName
     }
-    if(c == ">") {
-        return data
+    if(c == "/" || c == ">" || c == EOF) {
+        return afterAttributeName(c)
     }
     if(c == "=") {
         return beforeAttributeName
     }
-    return beforeAttributeName
+    currentAttribute = {
+        name: "",
+        value: ""
+    }
+    return attributeName(c)
+}
+
+function attributeName(c) {
+    //console.log(currentAttribute)
+    if(c.match(/^[\t\n\f ]$/) || c == "/" || c == ">" || c == EOF) {
+        return afterAttributeName(c)
+    }
+    if(c == "=") {
+        return beforeAttributeValue
+    } else if(c == "\u0000") {
+
+    } else if(c == "\"" || c == "'" || c == "<") {
+
+    } else {
+        currentAttribute.name += c
+        return attributeName
+    }
+}
+
+function beforeAttributeValue(c) {
+    if(c.match(/^[\t\n\f ]$/) || c == "/" || c == ">" || c == EOF) {
+        return beforeAttributeValue
+    }
+    if(c == "\"") {
+        return doubleQuotedAttributeValue
+    }
+    if(c == "\'") {
+        return singleQuotedAttributeValue
+    }
+    if(c == ">") {
+
+    } else {
+        return UnquotedAttributeValue(c)
+    }
+}
+
+function doubleQuotedAttributeValue(c) {
+    if(c == "\"") {
+        currentToken[currentAttribute.name] = currentAttribute.value
+        return afterQuotedAttributeValue
+    }
+    if(c == "\u0000") {
+
+    } else if(c == EOF) {
+
+    } else {
+        currentAttribute.value += c
+        return doubleQuotedAttributeValue
+    }
+}
+
+function afterQuotedAttributeValue(c) {
+    if(c.match(/^[\t\n\f ]$/)) {
+        return beforeAttributeName
+    } else if(c == "/") {
+        return selfClosingStartTag
+    } else if(c == ">") {
+        currentToken[currentAttribute.name] = currentAttribute.value
+        emit(currentToken)
+        return data
+    }
+    if(c == EOF) {
+
+    } else {
+        currentAttribute.value += c
+        return doubleQuotedAttributeValue
+    }
+}
+
+function UnquotedAttributeValue(c) {
+    if(c.match(/^[\t\n\f ]$/)) {
+        currentToken[currentAttribute.name] = currentAttribute.value
+    }
+    if(c == "/") {
+        currentToken[currentAttribute.name] = currentAttribute.value
+        return selfClosingStartTag
+    }
+    if(c == ">") {
+        currentToken[currentAttribute.name] = currentAttribute.value
+        emit(currentToken)
+        return data
+    }
+    if(c == "\u0000") {
+
+    } else if(c == "\"" || c == "'" || c == "<" || c == "=" || c == "'") {
+
+    } else if(c == EOF) {
+
+    } else {
+        currentAttribute.value += c
+        return UnquotedAttributeValue
+    }
+}
+
+function singleQuotedAttributeValue(c) {
+    if(c == "\'") {
+        currentToken[currentAttribute.name] = currentAttribute.value
+        return afterQuotedAttributeValue
+    }
+    if(c == "\u0000") {
+
+    } else if(c == EOF) {
+
+    } else {
+        currentAttribute.value += c
+        return singleQuotedAttributeValue
+    }
 }
 
 function selfClosingStartTag(c) {
-    if(c ==">") {
+    if(c == ">") {
         currentToken.isSelfClosing = true
+        emit(currentToken)
         return data
-    } else if(c == "EOF") {
+    }
+    return selfClosingStartTag
+}
+
+function endTagOpen(c) {
+    // 字母
+    if(c.match(/^[a-zA-Z]$/)) {
+        currentToken = {
+            type: "endTag",
+            tagName: ""
+        }
+        return tagName(c)
+    } else if(c == ">") {
+
+    } else if(c == EOF) {
 
     } else {
 
+    }
+}
+
+function afterAttributeName(c) {
+    if(c.match(/^[\t\n\f ]$/)) {
+        return afterAttributeName
+    }
+    if(c == "/") {
+        return selfClosingStartTag
+    }
+    if(c == "=") {
+        return beforeAttributeValue
+    }
+    if(c == ">") {
+        currentToken[currentAttribute.name] = currentAttribute.value
+        emit(currentToken)
+        return data
+    }
+    if(c == EOF) {
+
+    } else {
+        currentToken[currentAttribute.name] = currentAttribute.value
+        currentAttribute = {
+            name: "",
+            value: ""
+        }
+        return attributeName(c)
     }
 }
 
@@ -115,4 +290,5 @@ module.exports.parseHTML = function parseHTML(html) {
         state = state(c)
     }
     state = state(EOF)
+    console.log(stack[0])
 }
--- Week 09/browser/server.js
+++ Week 09/browser/server.js
@@ -17,8 +17,7 @@ const server = http.createServer((req, res) => {
     res.setHeader("Content-Type", "text/html")
     res.setHeader("X-Foo", "bar")
     res.writeHead(200, {'Content-Type': 'text/plain'})
-    res.end(`
-    <html maaa=a >
+    res.end(`<html maaa=a >
     <head>
         <style>
         body div #myid {

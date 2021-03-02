4753629083a72e26742ebecc501b0b6dea631e10
Sim <sim@snorl.ax>
2021-03-02 12:47:10 +0800

    9.10 计算选择器与元素匹配

--- Week 09/browser/parser.js
+++ Week 09/browser/parser.js
@@ -1,5 +1,4 @@
 const css = require("css")
-const { match } = require("assert")
 
 // 当前token
 let currentToken = null
@@ -16,7 +15,23 @@ function addCSSRules(text) {
 }
 
 function match(element, selector) {
+    if(!selector || !element.attributes)
+        return false
 
+    if(selector.charAt(0) == "#") {
+        var attr = element.attributes.filter(attr => attr.name === "id")[0]
+        if(attr && attr.value === selector.replace("#", ''))
+            return true
+    } else if(selector.charAt(0) == ".") {
+        var attr = element.attributes.filter(attr => attr.name === "class")[0]
+        if(attr && attr.value === selector.replace(".", ''))
+            return true
+    } else {
+        if(element.tagName === selector) {
+            return true
+        }
+    }
+    return false
 }
 
 function computeCSS(element) {
